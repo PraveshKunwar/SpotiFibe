03e0068bcd7710a037552c0dc12a534eeef1ebad
PraveshKunwar <praveshisawesome2017@gmail.com>
2020-10-29 22:15:44 -0400

    Made changes to navbar.

--- backend/main.js
+++ backend/main.js
@@ -1,17 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = (process.env.PORT = 5000);
+const PORT = 5000;
 
 const app = express();
-
-app.get("/api/customers", (req, res) => {
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
-  ];
-
-  res.json(customers);
-});
 
 app.listen(PORT, console.log(`Listening to ${PORT}`));
--- spotifibe/src/Components/Navbar.js
+++ spotifibe/src/Components/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import Home from "./Home";
 
 class Navbar extends React.Component {
   render() {
-    return <div className="navbar"></div>;
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            {/* <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} /> */}
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
