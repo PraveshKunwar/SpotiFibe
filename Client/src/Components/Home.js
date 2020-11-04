2b1c2a2f58e7c21b3d8a98586668048fdfc73fe5
PraveshKunwar <praveshisawesome2017@gmail.com>
2020-11-03 20:42:54 -0500

    Added grid and buttons.

--- Client/src/Components/Home.js
+++ Client/src/Components/Home.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { Animated } from "react-animated-css";
 import style from "./index.css";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { Container, Notification } from "react-bulma-components";
 import SpotiFibe from "../Images/spotifibe-cover.jpg";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
 
 class Home extends Component {
   render() {
@@ -18,29 +27,71 @@ class Home extends Component {
             <h2>
               The ultimate Spotify to <br></br>YouTube Playlist Converter!
             </h2>
-            <button variant="success" className="conversion">
-              <a href="/convert">Let's Convert!</a>
-            </button>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={logo} />
+              <Card.Body className>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Card Subtitle
+                </Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+            <Button variant="warning" size="lg" className="btn">
+              <a href="/convert">Start Converting</a>
+            </Button>
             <br></br>
-            <br></br>
-            <button variant="success" className="about-btn">
-              <a href="/about">About SpotiFibe.</a>
-            </button>
+            <Button variant="danger" size="lg" className="btn-2">
+              <a href="/about">About SpotiFibe</a>
+            </Button>
           </div>
         </Animated>
         <Animated animationIn="fadeIn" animationInDelay="3000" isVisible={true}>
-          <div className="container" style={style}>
-            <div className="1">
-              <h3>Headers</h3>
-              <p>asdjkhasjkdh</p>
+          <div className="grid-container">
+            <div className="one">
+              <br></br>
+              <h3>
+                <b>
+                  {" "}
+                  Q: What is <i>SpotiFibe?</i>
+                </b>
+              </h3>
+              <br></br>
+              <p>
+                A: Welcome to <b>SpotiFibe</b>! This website allows you to
+                convert Spotify playlists into one's for YouTube! Check out
+                Convert page to easily start converting them into one another!
+                Enjoy! :)
+              </p>
             </div>
-            <div className="2">
-              <h3>Headers</h3>
-              <p>asdjkhasjkdh</p>
+            <div className="two">
+              <br></br>
+              <h3>
+                <b>Q: How are they converted into one another?</b>
+              </h3>
+              <br></br>
+              <p>
+                A: Simple! Using Spotify's Web API, we used Node.JS to access
+                all of your playlists, and used YouTube's API to convert them
+                into the new YouTube playlists! Cool, right!
+              </p>
             </div>
-            <div className="3">
-              <h3>Headers</h3>
-              <p>asdjkhasjkdh</p>
+            <div className="three">
+              <br></br>
+              <h3>
+                <b>Q: Sweet! Where can I start converting?</b>
+              </h3>
+              <br></br>
+              <p>
+                To get started, click the button down below! Make sure to login
+                into your YouTube and Spotify accounts for authorization! The
+                Playlist will be shown on your YouTube account once done!
+              </p>
             </div>
           </div>
         </Animated>
