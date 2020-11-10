4b56943eedbe87b92d1a65df4fa4d22d42eac665
PraveshKunwar <praveshisawesome2017@gmail.com>
2020-11-10 08:46:37 -0500

    New commits.

--- backend/main.js
+++ backend/main.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 5000;
 const cors = require("cors");
+let querystring = require("querystring");
+const request = require("request");
+const redirect_uri =
+  process.env.redirect_uri || "http://localhost:5000/callback";
+require("dotenv").config();
 
-const app = express();
-app.use(cors());
-app.get("/auth/spotify", (req, res) => {
-  res.send("akdjaklsdjsakld");
-});
 app.listen(PORT, console.log(`Listening to ${PORT}`));
