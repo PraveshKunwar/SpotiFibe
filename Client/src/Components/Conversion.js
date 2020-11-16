feebd30ac07bb55114d6a47c437b0a01c740c780
PraveshKunwar <praveshisawesome2017@gmail.com>
2020-11-15 21:00:55 -0500

    Fixed Images and Text not loading.

--- Client/src/Components/Conversion.js
+++ Client/src/Components/Conversion.js
@@ -1,61 +1,88 @@
-import React, { useState, useEffect } from "react";
-import LoginBtns from "./LoginBtns";
+import React from "react";
 import { Animated } from "react-animated-css";
 import style from "./Conversion.css";
 import fetch from "node-fetch";
-import Playlists from "./Spotify-Components/Playlists";
 import ProfileHolder from "../Images/spotifylogo.png";
 
-const Successful = (props) => {
-  const [name, setName] = useState("User");
-  const [profileImage, setProfileImage] = useState(ProfileHolder);
-  const [profileUrl, setProfileUrl] = useState("");
-  const [playlistItems, setPlaylistItems] = useState([]);
-  useEffect(() => {
+class Successful extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "User",
+      profileUrl: false,
+      profileImage: <ProfileHolder />,
+      token: this.props.token,
+      items: [],
+    };
+  }
+  componentDidMount() {
+    document.title = "Convert | SpotiFibe";
     fetch("https://api.spotify.com/v1/me", {
       headers: {
-        Authorization: "Bearer " + props.token,
+        Authorization: "Bearer " + this.state.token,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setName(data.display_name);
-        setProfileImage(data.images[0].url);
-        setProfileUrl(data.external_urls.spotify);
+        this.setState({
+          name: data.display_name,
+          profileUrl: data.external_urls.spotify,
+          profileImage: data.images[0].url,
+        });
       })
       .catch((err) => console.error(err));
-    fetch("https://api.spotify.com/v1/me/playlists", {
+    fetch("https://api.spotify.com/v1/me/playlists?limit=50", {
       headers: {
-        Authorization: "Bearer " + props.token,
+        Authorization: "Bearer " + this.state.token,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setPlaylistItems(data);
+        this.setState({
+          items: data.items,
+        });
+        this.state.items.map((x) => {
+          console.log(x.name); // returns all the names of the playlists
+        });
       })
-      .catch((err) => console.error(err));
-  }, []);
 
-  return (
-    <div className="successful" style={style}>
-      <Animated isVisible={true} animationInDelay="1000" animationIn="fadeIn">
-        <div className="fetched-header">
-          <h1>Welcome {name}.</h1>
-        </div>
-      </Animated>
-      <Animated isVisible={true} animationInDelay="2000" animationIn="fadeIn">
-        <div className="profile">
-          <a href={profileUrl}>
-            <img className="profile-image" src={profileImage}></img>
-          </a>
-        </div>
-        <hr></hr>
-      </Animated>
-      <Animated isVisible={true} animationInDelay="3000" animationIn="fadeIn">
-        <div className="playlists"></div>
-      </Animated>
-    </div>
-  );
-};
+      .catch((err) => console.error(err));
+  }
+  render() {
+    return (
+      <div className="successful" style={style}>
+        <Animated isVisible={true} animationInDelay="1000" animationIn="fadeIn">
+          <div className="fetched-header">
+            <h1>Welcome {this.state.name}.</h1>
+          </div>
+        </Animated>
+        <Animated isVisible={true} animationInDelay="2000" animationIn="fadeIn">
+          <div className="profile">
+            <a href={this.state.profileUrl} target="_blank">
+              <img
+                className="profile-image"
+                src={this.state.profileImage}
+              ></img>
+            </a>
+          </div>
+          <hr></hr>
+        </Animated>
+        <Animated isVisible={true} animationInDelay="3000" animationIn="fadeIn">
+          <h2>Playlists</h2>
+          <div className="playlists">
+            {this.state.items.map((item) => {
+              return (
+                <div>
+                  <img src={item.images[0].url}></img>
+                  <p className="playlists-text">{item.name}</p>
+                </div>
+              );
+            })}
+          </div>
+        </Animated>
+      </div>
+    );
+  }
+}
 
 export default Successful;
--- Client/src/Components/Spotify-Components/Playlists.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import fetch from "node-fetch";
-
-const Playlists = (props) => {
-  return <div className="playlists-grid">askdjaskldjaskljd</div>;
-};
-
-export default Playlists;
